0be3ee3c8d77ab635c916ae428ffb72efb4561ec
karthik <karthik@karthiks-MacBook-Pro.local>
2024-03-08 14:35:52 -0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <svg width="900" height = "500">
+      <circle
+        cx ="480"
+        cy ="300"
+        r="200"
+        fill ="yellow"
+      />
+      </svg>
+    <Circle  />
     </div>
   );
 }
+const Circle =() => (
+  <div className="App">
+      <svg width="900" height = "500">
+      <circle
+        cx ="480"
+        cy ="300"
+        r="200"
+        fill ="yellow"
+      
+      />
+      </svg>
+
+    </div>
+);
+
 
 export default App;
